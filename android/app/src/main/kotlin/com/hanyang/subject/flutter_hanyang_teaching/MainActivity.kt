f1c9ea05fb6aea66bd3287741995d501baf112cd
Jeongin Lee <jil8885@hanyang.ac.kr>
2020-09-29 16:40:06 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/hanyang/subject/flutter_hanyang_teaching/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hanyang.subject.flutter_hanyang_teaching
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'page/homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '한양대 학과 정보 시스템',
+      home: HomePage(title: '한양대 학과 정보 시스템',),
+    );
+  }
+}
+
+
